80c1d00503a0c99c0dbc5dad2b3950af68db9d98
Paula Silva <paula.silva0507@hotmail.com>
2026-01-28 11:15:08 -0500

    Cambio de titulo del violín + eliminar precios negativos

--- Visualizaciones_bikepries_py.py
+++ Visualizaciones_bikepries_py.py
@@ -5,10 +5,13 @@ import seaborn as sns
 
 df = pd.read_csv("BikePrices.csv")
 
+#Eliminar los valores negativos en la columna Selling_Price
+df = df[df["Selling_Price"] >= 0]
+
 sns.histplot(data=df, x="Year")
 plt.title("Distribución del año de fabricación")
 plt.show()
 
 sns.violinplot(data=df, x="Owner", y="Selling_Price")
-plt.title("Precio de venta según número de dueños")
+plt.title("Precio de venta según número de dueños de la bicicleta")
 plt.show()
