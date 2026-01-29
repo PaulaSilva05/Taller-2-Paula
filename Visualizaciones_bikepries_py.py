db0c16f905bae026cb09b06101a79ac5c371cb49
Manuela Ramírez <manuelaiturralde@gmail.com>
2026-01-29 16:34:57 -0500

    MejorarViolin

--- Visualizaciones_bikepries_py.py
+++ Visualizaciones_bikepries_py.py
@@ -6,25 +6,27 @@ import seaborn as sns
 df = pd.read_csv("BikePrices.csv")
 
 #Eliminar los valores negativos en la columna Selling_Price
-df = df[df["Selling_Price"] >= 0]
+df2 = df[df["Selling_Price"] >= 0]
+print ((df["Selling_Price"] < 0).sum())
 
 #Histograma de año de fabricación
-sns.histplot(data=df, x="Year")
+sns.histplot(data=df2, x="Year")
 plt.title("Distribución del año de fabricación")
 plt.show()
 
 #Grafico de viol+in segun el numero de dueños y el precio
 sns.violinplot(
-    data=df,
+    data=df2,
     x="Owner",
     y="Selling_Price",
+    cut = 0,
     palette="Set2")
 
 plt.title("Precio de venta según número de dueños de la bicicleta")
 plt.show()
 
 #Grafico de dona para conocer la proporción de bicicletas por numero de dueño
-brand_counts = df['Owner'].value_counts()
+brand_counts = df2['Owner'].value_counts()
 cmap = plt.colormaps["tab20c"]
 fig, ax = plt.subplots()
 ax.pie(
